93eeff7e0ccec58fc6c231ec6527637021e33945
MustafaAltaie <mustafaphoto111@gmail.com>
2024-03-01 22:10:52 +0100

    Completed the code as mentioned

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,22 @@ let rText = document.getElementById('rText') as HTMLInputElement;
 let m = document.getElementById('mText') as HTMLOutputElement;
 let btn = document.getElementById('resultBtn') as HTMLButtonElement;
 let result: number;
+let p: number;
+let r: number;
+let n: number;
 
 btn.addEventListener('click', (): void => {
-    const p = parseInt(pText.value);
-    const r = parseInt(rText.value);
-    const n = parseInt(nText.value);
-
-    result = p * ((r * (1 + r)^n) / ((- + r)^n - 1));
-
-    m.textContent = (Math.round(result)).toString();
+    if(Number(pText.value) <= 0 || Number(rText.value) <= 0 || Number(nText.value) <= 0){
+        alert('Please input realistic amounts')
+    } else {
+        p = Number(pText.value);
+        r = Number(rText.value);
+        if(Number(nText.value) > 300)
+        alert('Please input a reasonable number, for example up to 30 year');
+        else {
+            n = Number(nText.value);
+            result = p * r * Math.pow(1 + r,n) / (Math.pow(1 + r,n) - 1);
+            m.textContent = `The monthly cost is : ${(Math.round(result)).toString()}`;
+        }
+    }
 });
